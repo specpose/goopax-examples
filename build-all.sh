5b4a6dd40eae65b16f97845f3b7ffe9f1e71ff12
Fassio Blatter <fassio@specpose.com>
2025-02-13 12:23:04 +0100

    Setting the GOOPAX_DRAW_USE_SDL2 USE flag WIP

--- build-all.sh
+++ build-all.sh
@@ -11,7 +11,7 @@ export CMAKE_BUILD_PARALLEL_LEVEL="$(getconf _NPROCESSORS_ONLN)"
 cmake "$@" $CMAKE_FLAGS -B build src
 cmake --build build --target build_glatter
 cmake --build build --target build_eigen
-cmake --build build --target build_sdl3
+cmake --build build --target build_sdl
 #cmake --build build --target build_opencv
 cmake --build build --target build_boost
 cmake build
--- build-for-android.sh
+++ build-for-android.sh
@@ -8,7 +8,7 @@ export CMAKE_BUILD_PARALLEL_LEVEL="$(getconf _NPROCESSORS_ONLN)"
 cmake "$@" -B build/$build_type/$ABI src
 cmake --build build/$build_type/$ABI --target build_glatter
 cmake --build build/$build_type/$ABI --target build_eigen
-cmake --build build/$build_type/$ABI --target build_sdl3
+cmake --build build/$build_type/$ABI --target build_sdl
 cmake --build build/$build_type/$ABI --target build_boost
 cmake --build build/$build_type/$ABI --target build_opencv
 cmake --build build/$build_type/$ABI
