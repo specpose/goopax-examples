b463d18ac883fd5ecdd5c048275b7814b30d37a2
Fassio Blatter <fassio@specpose.com>
2025-07-16 11:04:36 +0200

    Refactoring cmake build script to reconfigure on-the-fly

---
+++ build-for-android.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+export CMAKE_BUILD_PARALLEL_LEVEL="$(getconf _NPROCESSORS_ONLN)"
+
+#may require cmake>=3.21
+cmake "$@" -B build/$build_type/$ABI src
+cmake --build build/$build_type/$ABI --target build_glatter
+cmake --build build/$build_type/$ABI --target build_eigen
+cmake --build build/$build_type/$ABI --target build_sdl3
+cmake --build build/$build_type/$ABI --target build_boost
+cmake --build build/$build_type/$ABI --target build_opencv
+cmake --build build/$build_type/$ABI --config Release
+cmake --install build/$build_type/$ABI --prefix data_local_tmp
