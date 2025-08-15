e9149d0f97942f67fa62cb6c7cba634b84dea8ae
Ingo Josopait <josopait@goopax.com>
2025-08-16 01:35:58 +0200

    removing duplicate windows definitions (already defined in CMakeLists.txt) switching to runtime library 'MultiThreaded'

--- build-all.sh
+++ build-all.sh
@@ -2,10 +2,6 @@
 
 set -e
 
-if  [ "$(uname -o)" == "Msys" ]; then
-  CMAKE_FLAGS="$CMAKE_FLAGS -DCMAKE_CONFIGURATION_TYPES=Debug;Release -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreadedDebug"
-fi
-
 export CMAKE_BUILD_PARALLEL_LEVEL="$(getconf _NPROCESSORS_ONLN)"
 
 cmake "$@" $CMAKE_FLAGS -B build src
