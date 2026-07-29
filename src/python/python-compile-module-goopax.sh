3ea27ad5bcb21c1d8f7466d82249d359bbca03cc
Fassio Blatter <fassio@specpose.com>
2026-07-29 09:15:09 +0200

    REFACTORING cmake build

---
+++ src/python/python-compile-module-goopax.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+g++ -D'MODULE_LIBRARY=1' -shared -fPIC -std=c++17 `/usr/bin/python3-config --cflags` -I../../../goopax-6.1.0-Linux-x86_64/include/goopax-6.1 -I../../../goopax-6.1.0-Linux-x86_64/share/goopax/licenses -L../../../goopax-6.1.0-Linux-x86_64/lib64 `/usr/bin/python3-config --ldflags --embed` -lgoopax -o ../../build/python/$1`/usr/bin/python-config --extension-suffix` ./$1.cpp
